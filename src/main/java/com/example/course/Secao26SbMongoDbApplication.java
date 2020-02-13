e1b6a5a4c4ef052a998ba08ea60ff88661036c7b
Denis Leal <leal.denis@gmail.com>
2020-02-13 15:49:17 -0300

    Primeiro commit, usuário criado

---
+++ src/main/java/com/example/course/Secao26SbMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao26SbMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao26SbMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course/Secao26SbMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao26SbMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
